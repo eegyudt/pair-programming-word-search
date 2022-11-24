c79d9b422f5ae10fa4e918380ea8cea076afafc1
Eszter Egyud <eszter.egyud@telus.com>
2022-11-23 17:37:53 -0700

    wordsearch.js implemented in pair programming session with John O'Halloran

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//LHL wordsearch.js implemented in pair programming with John O'Halloran
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (letters.length > 0) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+
+      if (l.includes(word)) return true;
+    }
+
+    const verticalJoin = [];
+
+    for (let i = 0; i < letters[0].length; i++) {
+      verticalJoin.push([]);
+
+      for (let x = 0; x < letters.length; x++) {
+
+        verticalJoin[i].push(letters[x][i]);
+      }
+      console.log(verticalJoin);
+    }
+    console.log(verticalJoin);
+    const verticalJoinCheck = verticalJoin.map(ls => ls.join(''));
+    for (const l of verticalJoinCheck) {
+      if (l.includes(word)) return true;
+    }
+
+    return false;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
